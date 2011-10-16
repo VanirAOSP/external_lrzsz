933bee7dcc5c45af88644998e9ba0aec165ea1bd
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-10-16 13:14:03 +0159

    lrzsz: Set LOCAL_MODULE_TAGS to optional instead of leaving it blank

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := lrz.c timing.c zperr.c zreadline.c crctab.c rbsb.c zm.c protname.c tcp.c lsyslog.c canit.c long-options.c xstrtol.c xstrtoul.c error.c
-LOCAL_MODULE_TAGS :=
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := rz
 LOCAL_CFLAGS := -DNFGVMIN -DHAVE_CONFIG_H=1
 LOCAL_SHARED_LIBRARIES := libc
@@ -12,7 +12,7 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := lsz.c timing.c zperr.c zreadline.c crctab.c rbsb.c zm.c protname.c tcp.c lsyslog.c canit.c long-options.c xstrtol.c xstrtoul.c error.c
-LOCAL_MODULE_TAGS :=
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := sz
 LOCAL_CFLAGS := -DNFGVMIN -DHAVE_CONFIG_H=1
 LOCAL_SHARED_LIBRARIES := libc
